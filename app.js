e2b0475dc255419c0160863e7d2f6c78cfbda12d
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2020-08-18 11:02:08 +0300

    connect mongo, add models for user and card

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cardsRouter = require('./routes/cards.js');
 const usersRouter = require('./routes/users.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cardsRouter);
 app.use('/', usersRouter);
---
+++ models/card.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator: (v) => v.match(/^https?:\/\/(www\.)?[\w./-]{1,}/),
+      message: (props) => `${props.value} неверный формат Url`,
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'user',
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
