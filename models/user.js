f101330d91ae447752d4a7fa066925aaf57e88ad
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2020-08-27 12:37:45 +0300

    API doesn't return a password hash, now

--- models/user.js
+++ models/user.js
@@ -40,10 +40,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
 });
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
